e5a3e95240b574305ea13016ce658e570d842dd6
Omar Houmz <omar.houmz@schibsted.com>
2019-04-13 23:50:04 +0100

    refactor header: get the nav links form config

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -1,74 +1,51 @@
-import { Link } from 'gatsby'
-import PropTypes from 'prop-types'
 import React from 'react'
+import { Link, StaticQuery, graphql } from 'gatsby'
 
 import './header.css'
 
-const Header = ({ siteTitle }) => (
-  <header class="header">
-    <div class="container-md">
-      <h1 class="website-title">
-        <Link
-          to="/old"
-          class="link-logo"
-          aria-label="omar houmz"
-          role="heading"
-          aria-level="1"
-        >
-          oh
-        </Link>
-      </h1>
-      <nav class="navigation dark">
-        <ul>
-          <li>
-            <Link to="/old" class="active">
-              Home
+const Header = () => (
+  <StaticQuery
+    query={graphql`
+      query SiteTitleQuery {
+        site {
+          siteMetadata {
+            menuLinks {
+              name
+              link
+            }
+          }
+        }
+      }
+    `}
+    render={data => (
+      <header className="header">
+        <div className="container-md">
+          <h1 className="website-title">
+            <Link
+              to="/"
+              className="link-logo"
+              aria-label="omar houmz"
+              role="heading"
+              aria-level="1"
+            >
+              oh
             </Link>
-          </li>
-          <li>
-            <Link to="./about">About</Link>
-          </li>
-          <li>
-            <Link to="./projects">Projects</Link>
-          </li>
-        </ul>
-      </nav>
-    </div>
-  </header>
-  // <header
-  //   style={{
-  //     background: `rebeccapurple`,
-  //     marginBottom: `1.45rem`,
-  //   }}
-  // >
-  //   <div
-  //     style={{
-  //       margin: `0 auto`,
-  //       maxWidth: 960,
-  //       padding: `1.45rem 1.0875rem`,
-  //     }}
-  //   >
-  //     <h1 style={{ margin: 0 }}>
-  //       <Link
-  //         to="/"
-  //         style={{
-  //           color: `white`,
-  //           textDecoration: `none`,
-  //         }}
-  //       >
-  //         {siteTitle}
-  //       </Link>
-  //     </h1>
-  //   </div>
-  // </header>
+          </h1>
+          <nav className="navigation dark">
+            <ul>
+              {data.site.siteMetadata.menuLinks.map((menuLink, index) => (
+                <li key={index}>
+                  <Link to={menuLink.link} activeClassName="active">
+                    {menuLink.name}
+                  </Link>
+                </li>
+              ))}
+            </ul>
+          </nav>
+        </div>
+      </header>
+    )}
+  />
 )
-
-Header.propTypes = {
-  siteTitle: PropTypes.string,
-}
-
-Header.defaultProps = {
-  siteTitle: ``,
-}
 
 export default Header
