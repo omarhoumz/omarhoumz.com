7f07deffd8870b165d364a3d90754569743ac104
Omar Houmz <omar.houmz@schibsted.com>
2019-04-13 23:47:01 +0100

    remove StaticQuery from layout.js

--- src/components/layout/layout.js
+++ src/components/layout/layout.js
@@ -1,37 +1,16 @@
-/**
- * Layout component that queries for data
- * with Gatsby's StaticQuery component
- *
- * See: https://www.gatsbyjs.org/docs/static-query/
- */
-
 import React from 'react'
 import PropTypes from 'prop-types'
-import { StaticQuery, graphql } from 'gatsby'
 
 import Header from '../header/header'
 import { Footer } from '../footer/footer'
 import './layout.css'
 
 const Layout = ({ children }) => (
-  <StaticQuery
-    query={graphql`
-      query SiteTitleQuery {
-        site {
-          siteMetadata {
-            title
-          }
-        }
-      }
-    `}
-    render={data => (
-      <>
-        <Header siteTitle={data.site.siteMetadata.title} />
-        <main className="main">{children}</main>
-        <Footer />
-      </>
-    )}
-  />
+  <>
+    <Header />
+    <main className="main">{children}</main>
+    <Footer />
+  </>
 )
 
 Layout.propTypes = {
