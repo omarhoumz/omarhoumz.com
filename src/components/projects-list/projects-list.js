d3b395c7bb112e97ee4ccae1e519696cfdea5e85
Omar Houmz <omar.houmz@schibsted.com>
2019-04-17 23:39:48 +0100

    export projects-list styles

--- src/components/projects-list/projects-list.js
+++ src/components/projects-list/projects-list.js
@@ -2,6 +2,7 @@ import React from 'react'
 import { StaticQuery, graphql } from 'gatsby'
 
 import { Link } from '../link/link'
+import './projects-list.css'
 
 export const ProjectsList = () => {
   return (
