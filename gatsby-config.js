c133ed892702e393c42bb57f8605bdce6627c4e0
Omar Houmz <omar.houmz@schibsted.com>
2019-04-27 20:02:48 +0100

    feat: add google analytics

--- gatsby-config.js
+++ gatsby-config.js
@@ -57,6 +57,12 @@ module.exports = {
         icon: `src/images/icon.png`, // This path is relative to the root of the site.
       },
     },
+    {
+      resolve: `gatsby-plugin-google-analytics`,
+      options: {
+        trackingId: 'UA-35979647-3',
+      },
+    },
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
