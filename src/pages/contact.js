96cbf388fcbad0604c3c75d84c65a9db3f8199d7
Omar Houmz <omar.houmz@schibsted.com>
2019-04-28 21:59:22 +0100

    feat: add contact.js to pages

---
+++ src/pages/contact.js
@@ -0,0 +1,23 @@
+import React from 'react'
+
+import Layout from '../components/layout/layout'
+import { ContactPage } from '../components/contact-page/contact-page'
+import SEO from '../components/seo'
+
+export default class Contact extends React.Component {
+  render() {
+    return (
+      <Layout>
+        <SEO
+          title="Contact"
+          keywords={[`Omar Houmz`, `Omar Houmz website`, `About`]}
+        />
+        <section className="section section-contact">
+          <div className="container-md">
+            <ContactPage />
+          </div>
+        </section>
+      </Layout>
+    )
+  }
+}
