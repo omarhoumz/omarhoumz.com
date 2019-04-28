484f09530af4d1f0e58998e33acd8a187857c4e1
Omar Houmz <omar.houmz@schibsted.com>
2019-04-28 21:56:54 +0100

    style: about description

--- src/components/description/description.jsx
+++ src/components/description/description.jsx
@@ -1,42 +1,34 @@
 import React from 'react'
 
+import { Link } from '../link/link'
+import './description.css'
+
 export const Description = () => {
   return (
     <React.Fragment>
-      <p>
+      <p className="p">
         I'm <strong>Omar Houmz</strong>, I'm a software engineer based in
         Casablanca, Morocco. I work at{' '}
-        <a
-          href="https://www.avito.ma/"
-          rel="noopener noreferrer"
-          target="_blank"
-          class="dark p"
-        >
+        <Link href="https://www.avito.ma/" className="dark inline">
           Avito.ma
-        </a>{' '}
+        </Link>{' '}
         as a <strong>front-end engineer</strong>.
       </p>
 
-      <p>
+      <p className="p">
         I studied a lot of disciplines in computer and data sciences. But I
         always liked <strong>front-end development</strong> and I still do.
       </p>
 
-      <p>
+      <p className="p">
         I'm actively helping my <strong>dev community</strong> to get better
         through events and conferences.
       </p>
 
-      <p>
+      <p className="p">
         If you want to help, or know someone that can support the work we do in
         our community, <strong>drop me a line</strong>:{' '}
-        <a
-          href="mailto:omarhoumz@gmail.com"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          omarhoumz@gmail.com
-        </a>
+        <Link href="mailto:omarhoumz@gmail.com">omarhoumz@gmail.com</Link>
       </p>
     </React.Fragment>
   )
