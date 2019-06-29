8f7f4a51ed01e64e7366c5dae07297618609ccab
Omar Houmz <omar.houmz@schibsted.com>
2019-06-29 19:56:29 +0100

    chore: inline link style

--- src/components/short-description/short-description.jsx
+++ src/components/short-description/short-description.jsx
@@ -6,22 +6,27 @@ const ShortDescription = () => (
   <React.Fragment>
     <p className="p">
       <span>I&apos;m</span>
+      {' '}
       <strong>Omar Houmz</strong>
       <span>
         , I&apos;m a software engineer based in Casablanca, Morocco. I work at
       </span>
+      {' '}
       <Link href="https://www.avito.ma/" className="dark inline">
         Avito.ma
       </Link>
+      {' '}
       <span>as a</span>
+      {' '}
       <strong>front-end engineer</strong>
       <span>.</span>
     </p>
 
     <p className="p">
       Wanna collaborate?
-      <strong>drop me a line</strong>
+      <strong> drop me a line</strong>
       <span>:</span>
+      {' '}
       <a
         href="mailto:omarhoumz@gmail.com"
         target="_blank"
