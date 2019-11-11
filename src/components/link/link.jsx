8b3745165d64bc7a752c2d6d79eb50909d311b4e
Omar Houmz <omarhoumz@gmail.com>
2019-11-11 23:46:24 +0100

    feat: refactor link, css modules and link gatsby

--- src/components/link/link.jsx
+++ src/components/link/link.jsx
@@ -1,33 +1,68 @@
 import React from 'react'
 import PropTypes from 'prop-types'
+import { Link as GatsbyLink } from 'gatsby'
+import cx from 'classnames'
+
+import styles from './link.module.css'
 
 const Link = ({
   href,
   target,
   children,
+  internal,
+  className,
+  variant,
+  inline,
   ...otherProps
 }) => {
   const anchorProps = {
+    className: cx(
+      styles.link,
+      {
+        [styles.dark]: variant === 'dark',
+        [styles.inline]: inline,
+      },
+      className,
+    ),
     href,
-    target: '_blank',
-    rel: 'noopener noreferrer',
     children,
     ...otherProps,
   }
+
+  let Comp = 'a'
+
+  if (target === '_blank') {
+    anchorProps.target = '_blank'
+    anchorProps.rel = 'noopener noreferrer'
+  }
+
+  if (internal) {
+    Comp = GatsbyLink
+    anchorProps.to = href
+  }
+
   // eslint-disable-next-line react/jsx-props-no-spreading
-  return <a {...anchorProps}>{children}</a>
+  return <Comp {...anchorProps}>{children}</Comp>
 }
 
 Link.defaultProps = {
   href: '#!',
   target: null,
   children: null,
+  internal: false,
+  className: '',
+  variant: 'default',
+  inline: false,
 }
 
 Link.propTypes = {
   href: PropTypes.string,
   target: PropTypes.string,
   children: PropTypes.node,
+  internal: PropTypes.bool,
+  className: PropTypes.string,
+  variant: PropTypes.oneOf(['default', 'dark']),
+  inline: PropTypes.bool,
 }
 
 export default Link
