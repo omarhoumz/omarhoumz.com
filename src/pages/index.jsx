32d5a80783de3c6a53890d07256771e07f03d361
Omar Houmz <omarhoumz@gmail.com>
2019-11-16 22:19:56 +0100

    feat: use section component

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -5,34 +5,35 @@ import Layout from '../components/layout/layout'
 import SEO from '../components/seo'
 import ProjectsList from '../components/projects-list/projects-list'
 import ShortDescription from '../components/short-description/short-description'
+import Section from '../components/_ui/section/section'
 
 const Home = () => (
   <Layout>
     <SEO title="Home" keywords={['Omar Houmz', 'Omar Houmz website', 'Home']} />
-    <section className="section section-home-about">
-      <div className="container-md">
-        <h2 className="section-title">Hi,</h2>
+    <Section
+      title="Hi,"
+      classes={{
+        inner: 'container-md',
+      }}
+    >
+      <ShortDescription />
 
-        <div className="about">
-          <ShortDescription />
+      <Link href="/about" internal btnStyle>
+        More about me ...
+      </Link>
+    </Section>
+    <Section
+      title="Latest Projects"
+      classes={{
+        inner: 'container-md',
+      }}
+    >
+      <ProjectsList latestOnly />
 
-          <Link href="/about" internal>
-            More about me ...
-          </Link>
-        </div>
-      </div>
-    </section>
-    <section className="section section-home-projects">
-      <div className="container-md">
-        <h2 className="section-title">Latest Projects</h2>
-
-        <ProjectsList latestOnly />
-
-        <Link href="/projects" internal>
-          See all projects
-        </Link>
-      </div>
-    </section>
+      <Link href="/projects" internal btnStyle>
+        See all projects
+      </Link>
+    </Section>
   </Layout>
 )
 
