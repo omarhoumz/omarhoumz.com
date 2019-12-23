edce69cb0592a241df4d223e04592233f8e414b4
Omar Houmz <omarhoumz@gmail.com>
2019-12-23 23:52:19 +0100

    chore: header to css module

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -1,33 +1,35 @@
 import React from 'react'
+import cx from 'classnames'
 
 import menuLinks from './header.queries'
 import Link from '../link/link'
-import './header.css'
+import styles from './header.module.css'
 
 const Header = () => {
   const { site } = menuLinks()
 
   return (
-    <header className="header">
-      <div className="container-md">
-        <h1 className="website-title">
+    <header className={styles.header}>
+      <div className={cx('container-md', styles.innerHeader)}>
+        <h1 className={styles.websiteTitle}>
           <Link
-            href="/"
-            className="link-logo"
-            aria-label="omar houmz"
-            role="heading"
-            aria-level="1"
+            href='/'
+            className={styles.linkLogo}
+            aria-label='Omar Houmz'
+            role='heading'
+            aria-level='1'
           >
             oh
           </Link>
         </h1>
-        <nav className="navigation dark">
+        <nav className={cx(styles.navigation, styles.dark)}>
           {site.siteMetadata.menuLinks.map((menuLink, index) => (
             <Link
               href={menuLink.link}
               internal
               key={index.toString()}
-              activeClassName="active"
+              activeClassName={styles.active}
+              className={styles.navAnchor}
             >
               {menuLink.name}
             </Link>
