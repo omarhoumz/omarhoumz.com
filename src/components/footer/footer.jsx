b024d4848b1beed40290f4fe543e62ea60b4ef8a
Omar Houmz <omarhoumz@gmail.com>
2019-12-23 23:22:44 +0100

    chore: footer css modules

--- src/components/footer/footer.jsx
+++ src/components/footer/footer.jsx
@@ -1,33 +1,32 @@
 import React from 'react'
 
 import Link from '../link/link'
-import './footer.css'
+import styles from './footer.module.css'
 
-const Footer = () => {
-  const currentYear = new Date().getFullYear()
-  return (
-    <footer className="footer">
-      <div className="container-md">
-        <p className="copy">
-          <small>
-            &copy; (not really)&nbsp;
-            <span id="current-year">{currentYear}</span>
-            <Link
-              href="https://github.com/omarhoumz/omarhoumz.github.io"
-              variant="dark"
-            >
-              by Omar Houmz
-            </Link>
-          </small>
-        </p>
-        <nav className="navigation">
-          <Link href="https://www.linkedin.com/in/omarhoumz/">LinkedIn</Link>
-          <Link href="https://github.com/omhoumz">Github</Link>
-          <Link href="mailto:omarhoumz@gmail.com">Email</Link>
-        </nav>
-      </div>
-    </footer>
-  )
-}
+const currentYear = new Date().getFullYear()
+
+const Footer = () => (
+  <footer className={styles.footer}>
+    <div className='container-md'>
+      <p className={styles.copy}>
+        <small>
+          &copy; (not really)&nbsp;
+          {currentYear}
+          <Link
+            href='https://github.com/omarhoumz/omarhoumz.github.io'
+            variant='dark'
+          >
+            by Omar Houmz
+          </Link>
+        </small>
+      </p>
+      <nav className={styles.navigation}>
+        <Link href='https://www.linkedin.com/in/omarhoumz/'>LinkedIn</Link>
+        <Link href='https://github.com/omhoumz'>Github</Link>
+        <Link href='mailto:omarhoumz@gmail.com'>Email</Link>
+      </nav>
+    </div>
+  </footer>
+)
 
 export default Footer
