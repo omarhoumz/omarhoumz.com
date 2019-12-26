49b9aa827435506dd1b56547cc04511a9c3a6907
Omar Houmz <omarhoumz@gmail.com>
2019-12-26 22:34:06 +0100

    chore: contact page revamped

--- src/components/contact-page/contact-page.jsx
+++ src/components/contact-page/contact-page.jsx
@@ -1,30 +1,90 @@
 import React from 'react'
+import PropTypes from 'prop-types'
 
 import Link from '../link/link'
 import styles from './contact-page.module.css'
 
+const contactLinks = [
+  {
+    label: 'Through email:',
+    links: [
+      {
+        label: 'omarhoumz@gmail.com',
+        link: 'mailto:omarhoumz@gmail.com',
+        external: true,
+      },
+      {
+        label: 'contact@omarhoumz.com',
+        link: 'mailto:contact@omarhoumz.com',
+        external: true,
+      },
+    ],
+  },
+  {
+    label: 'Twitter:',
+    links: [
+      {
+        label: '@omarhoumz',
+        link: 'https://twitter.com/omarhoumz',
+        external: true,
+      },
+    ],
+  },
+  {
+    label: 'Or LinkedIn:',
+    links: [
+      {
+        label: '/in/omarhoumz',
+        link: 'https://www.linkedin.com/in/omarhoumz/',
+        external: true,
+      },
+    ],
+  },
+]
+
 const ContactPage = () => (
   <ul className={styles.contactInfo}>
-    <li>
-      <span>Through email:</span>
-      <ul>
-        <li>
-          <Link href='mailto:omarhoumz@gmail.com'>omarhoumz@gmail.com</Link>
-        </li>
-        <li>
-          <Link href='mailto:contact@omarhoumz.com'>contact@omarhoumz.com</Link>
-        </li>
-      </ul>
-    </li>
-    <li>
-      <span>Twitter: </span>
-      <Link href='https://twitter.com/omarhoumz'>@omarhoumz</Link>
-    </li>
-    <li>
-      <span>Or LinkedIn: </span>
-      <Link href='https://www.linkedin.com/in/omarhoumz/'>@omarhoumz</Link>
-    </li>
+    {contactLinks.map(({ label, links }) => (
+      <ContactItem label={label} links={links} />
+    ))}
   </ul>
 )
+
+const linkType = {
+  label: PropTypes.string.isRequired,
+  link: PropTypes.string.isRequired,
+  external: PropTypes.bool.isRequired,
+}
+
+const ContactItem = ({ label: title, links }) => (
+  <li className={styles.contactItem}>
+    <h3 className={styles.linksTitle}>{title}</h3>
+    <ul className={styles.linksList}>
+      {links.map(({ label, link, external }) => (
+        <ContactLink link={link} label={label} external={external} />
+      ))}
+    </ul>
+  </li>
+)
+
+ContactItem.propTypes = {
+  label: PropTypes.string.isRequired,
+  links: PropTypes.arrayOf(linkType).isRequired,
+}
+
+const ContactLink = ({ label, link, external }) => (
+  <li className={styles.linkItem}>
+    <Link
+      href={link}
+      internal={!external}
+      className={styles.link}
+      target='_blank'
+    >
+      {label}
+    </Link>
+  </li>
+)
+
+ContactLink.propTypes = linkType
 
 export default ContactPage
