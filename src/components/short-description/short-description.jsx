f87da5ac10db55fd39e969afdff1fc1eaf5120ef
Omar Houmz <omarhoumz@gmail.com>
2020-01-05 10:56:37 +0100

    chore: use mdx for short desc

--- src/components/short-description/short-description.jsx
+++ src/components/short-description/short-description.jsx
@@ -1,36 +1,25 @@
 import React from 'react'
+import { MDXProvider } from '@mdx-js/react'
 
+import ShortDesc from './short-description.mdx'
 import Link from '../link/link'
 
-const ShortDescription = () => (
-  <>
-    <p className='p'>
-      <span>I&apos;m</span>
-      &nbsp;
-      <strong>Omar Houmz</strong>
-      <span>
-        , I&apos;m a software engineer based in Casablanca, Morocco. I work at
-      </span>
-      &nbsp;
-      <Link href='https://www.avito.ma/' variant='dark'>
-        Avito.ma
-      </Link>
-      <span>as a</span>
-      &nbsp;
-      <strong>front-end engineer</strong>
-      <span>.</span>
-    </p>
+// eslint-disable-next-line react/jsx-props-no-spreading
+const P = props => <p className='p' {...props} />
+const Anchor = props => (
+  // eslint-disable-next-line react/jsx-props-no-spreading
+  <Link variant='dark' target='_blank' {...props} />
+)
 
-    <p className='p'>
-      Wanna collaborate?
-      <strong> drop me a line</strong>
-      <span>:</span>
-      &nbsp;
-      <Link href='mailto:omarhoumz@gmail.com' target='_blank'>
-        omarhoumz@gmail.com
-      </Link>
-    </p>
-  </>
+const ShortDescription = () => (
+  <MDXProvider
+    components={{
+      p: P,
+      a: Anchor,
+    }}
+  >
+    <ShortDesc />
+  </MDXProvider>
 )
 
 export default ShortDescription
