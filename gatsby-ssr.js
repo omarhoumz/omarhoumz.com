572829611925733c899103f7503ffe20f1545bb1
Omar Houmz <omarhoumz@gmail.com>
2020-01-26 20:59:34 +0100

    chore: add wrap root element

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -5,3 +5,7 @@
  */
 
 // You can delete this file if you're not using it
+import { wrapRootElement as wrap } from './wrap-root-element'
+
+// eslint-disable-next-line import/prefer-default-export
+export const wrapRootElement = wrap
---
+++ wrap-root-element.jsx
@@ -0,0 +1,34 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import { MDXProvider } from '@mdx-js/react'
+import Blockquote from './src/components/_ui/mdx/blockquote/blockquote'
+// import { Code } from './src/components/code'
+// import { preToCodeBlock } from 'mdx-utils'
+
+// components is its own object outside of render so that the references to
+// components are stable
+const components = {
+  // eslint-disable-next-line react/jsx-props-no-spreading
+  pre: preProps => <pre {...preProps} />,
+  blockquote: props => (
+    <Blockquote
+      // eslint-disable-next-line react/jsx-props-no-spreading
+      {...props}
+    />
+  ),
+  // const props = preToCodeBlock(preProps)
+  // // if there's a codeString and some props, we passed the test
+  // if (props) {
+  //   return <Code {...props} />
+  // } else {
+  //   // it's possible to have a pre without a code in it
+  // }
+}
+
+// eslint-disable-next-line import/prefer-default-export
+export const wrapRootElement = ({ element }) => (
+  <MDXProvider components={components}>{element}</MDXProvider>
+)
+wrapRootElement.propTypes = {
+  element: PropTypes.oneOfType([PropTypes.node, PropTypes.string]).isRequired,
+}
