7afdbae2340a0899c5c3a4079a2a45163cf8e2dc
Omar Houmz <omarhoumz@gmail.com>
2020-01-26 21:02:30 +0100

    refactor: footer section

--- src/components/footer/footer.jsx
+++ src/components/footer/footer.jsx
@@ -5,29 +5,58 @@ import styles from './footer.module.css'
 
 const currentYear = new Date().getFullYear()
 
-const Footer = () => (
-  <footer className={styles.footer}>
-    <div className='container-md'>
-      <p className={styles.copy}>
-        <small>
-          &copy; (not really)&nbsp;
-          {currentYear}
-          &nbsp;&nbsp;
-          <Link
-            href='https://github.com/omarhoumz/omarhoumz.github.io'
-            variant='dark'
-          >
-            by Omar Houmz
-          </Link>
-        </small>
-      </p>
-      <nav>
-        <Link href='https://www.linkedin.com/in/omarhoumz/'>LinkedIn</Link>
-        <Link href='https://github.com/omhoumz'>Github</Link>
-        <Link href='mailto:omarhoumz@gmail.com'>Email</Link>
-      </nav>
-    </div>
-  </footer>
-)
+const footerNavLinks = [
+  {
+    label: 'LinkedIn',
+    link: 'https://www.linkedin.com/in/omarhoumz/',
+    internal: false,
+  },
+  {
+    label: 'Github',
+    link: 'https://github.com/omhoumz',
+    internal: false,
+  },
+  {
+    label: 'Email',
+    link: 'mailto:omarhoumz@gmail.com',
+    internal: false,
+  },
+]
+
+const Footer = () => {
+  const copyText = `(not really) ${currentYear} `
+
+  return (
+    <footer className={styles.footer}>
+      <div className='container-md'>
+        <p className={styles.copy}>
+          <small>
+            &copy;&nbsp;
+            {copyText}
+            <Link
+              href='https://github.com/omarhoumz/omarhoumz.github.io'
+              inline
+              className={styles.navLink}
+            >
+              by Omar Houmz
+            </Link>
+          </small>
+        </p>
+        <nav>
+          {footerNavLinks.map(({ link, label, internal }, index) => (
+            <Link
+              href={link}
+              className={styles.navLink}
+              internal={internal}
+              key={index.toString()}
+            >
+              {label}
+            </Link>
+          ))}
+        </nav>
+      </div>
+    </footer>
+  )
+}
 
 export default Footer
