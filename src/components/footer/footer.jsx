45e6b4deaa9c35f2a412bfd3fafc2e0a98ee9886
Omar Houmz <omhoumz@gmail.com>
2020-05-28 23:11:22 +0000

    chore: layout styles

--- src/components/footer/footer.jsx
+++ src/components/footer/footer.jsx
@@ -5,44 +5,35 @@ import styles from './footer.module.css'
 
 const currentYear = new Date().getFullYear()
 
-const footerNavLinks = [
-  {
-    label: 'LinkedIn',
-    link: 'https://www.linkedin.com/in/omarhoumz/',
-    internal: false,
-  },
-  {
-    label: 'Github',
-    link: 'https://github.com/omhoumz',
-    internal: false,
-  },
-  {
-    label: 'Email',
-    link: 'mailto:omarhoumz@gmail.com',
-    internal: false,
-  },
-]
+// const footerNavLinks = [
+//   {
+//     label: 'LinkedIn',
+//     link: 'https://www.linkedin.com/in/omarhoumz/',
+//     internal: false,
+//   },
+//   {
+//     label: 'Github',
+//     link: 'https://github.com/omhoumz',
+//     internal: false,
+//   },
+//   {
+//     label: 'Email',
+//     link: 'mailto:omarhoumz@gmail.com',
+//     internal: false,
+//   },
+// ]
+
+const NotesLink = ({ href, children }) => (
+  <Link inline variant='dark' href={href} target='_blank' nofollow>
+    {children}
+  </Link>
+)
 
 const Footer = () => {
   return (
     <footer className={styles.footer}>
-      <div className='container-md'>
-        <p className={styles.copy}>
-          <small>
-            &copy;&nbsp;
-            {currentYear}&nbsp;
-            <Link
-              href='https://github.com/omarhoumz/omarhoumz.github.io'
-              inline
-              className={styles.navLink}
-              target='_blank'
-              nofollow
-            >
-              by Omar Houmz
-            </Link>
-          </small>
-        </p>
-        <nav>
+      <div className={['container-md', styles.innerFooter].join(' ')}>
+        {/* <nav>
           {footerNavLinks.map(({ link, label, internal }, index) => (
             <Link
               href={link}
@@ -55,7 +46,18 @@ const Footer = () => {
               {label}
             </Link>
           ))}
-        </nav>
+        </nav> */}
+        <p className={styles.copy}>Omar Houmz &copy; {currentYear}</p>
+        <small>
+          This site is built with{' '}
+          <NotesLink href='https://www.gatsbyjs.org/'>Gatsby</NotesLink> and
+          hosted on <NotesLink href='https://netlify.com/'>Netlify</NotesLink>.
+          The source code is hosted on{' '}
+          <NotesLink href='https://github.com/omarhoumz/omarhoumz.github.io'>
+            Github
+          </NotesLink>
+          .
+        </small>
       </div>
     </footer>
   )
