cff943642e067c26c4ce4b287676fb0a44b07cba
Omar Houmz <omarhoumz@gmail.com>
2020-12-15 22:57:33 +0100

    feat: add contact page content

--- pages/contact.jsx
+++ pages/contact.jsx
@@ -1,4 +1,5 @@
 import Head from 'next/head'
+import Link from 'src/components/link/link'
 
 import Layout from 'src/layout/layout'
 
@@ -12,9 +13,27 @@ function Contact() {
 
       <Layout mainClassName='py-14 space-y-12'>
         <section className='block xl:w-full xl:max-w-2xl xl:mx-auto'>
-          <div className='flex flex-col items-start space-y-6 px-5 xl:px-0'>
-            <h1 className='text-3xl font-bold'>Contact</h1>
-            <div className='grid'></div>
+          <div className='flex flex-col items-start space-y-6 px-5 xl:px-0 text-blueGray-600'>
+            <h1 className='text-3xl text-blueGray-800 font-bold'>Contact</h1>
+            <p>If you want to get in touch, this is where I hang out often.</p>
+            <div>
+              <h2>Twitter:</h2>
+              <Link external nofollow href='https://twitter.com/omarhoumz'>
+                @omarhoumz
+              </Link>
+            </div>
+            <div>
+              <h2>LinkedIn:</h2>
+              <Link external nofollow href='https://linkedin.com/in/omarhoumz'>
+                /in/omarhoumz
+              </Link>
+            </div>
+            <div>
+              <h2>Email:</h2>
+              <Link external nofollow href='mailto://omarhoumz@gmail.com'>
+                omarhoumz[at]gmail.com
+              </Link>
+            </div>
           </div>
         </section>
       </Layout>
