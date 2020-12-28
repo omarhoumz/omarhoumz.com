a81ac3a47f30b558fb1b2efc688ff05dbdd663b0
Omar Houmz <omarhoumz@gmail.com>
2020-12-28 19:59:55 +0100

    chore: add google analytics

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,25 @@
 import { DefaultSeo } from 'next-seo'
+import { useEffect } from 'react'
 import Head from 'next/head'
+import { useRouter } from 'next/router'
 
+import * as gtag from '@/lib/gtag'
 import SEO from 'next-seo.config'
 
 import 'styles/globals.css'
 
 function MyApp({ Component, pageProps }) {
+  const router = useRouter()
+  useEffect(() => {
+    const handleRouteChange = (url) => {
+      gtag.pageview(url)
+    }
+    router.events.on('routeChangeComplete', handleRouteChange)
+    return () => {
+      router.events.off('routeChangeComplete', handleRouteChange)
+    }
+  }, [router.events])
+
   return (
     <>
       <DefaultSeo {...SEO} />
