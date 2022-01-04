bc9c43a71e3b2f0576e125c15ce24c9b798a126d
Omar Houmz <omarhoumz@gmail.com>
2022-01-04 21:59:49 +0100

    chore:

--- src/components/pill/pill.jsx
+++ src/components/pill/pill.jsx
@@ -2,7 +2,7 @@ export default function Pill({ label, className = '' }) {
   return (
     <span
       className={[
-        'text-xs text-gray-500 font-medium tracking-wide py-1 px-3 rounded-full uppercase bg-gray-100',
+        'text-xs text-gray-500 font-medium tracking-wide py-1 px-3 rounded-full uppercase bg-gray-100 relative',
         className,
       ]
         .filter(Boolean)
