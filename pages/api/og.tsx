f9abf948de71e11c7db544da62f56e0e996d2cde
Omar Houmz <omarhoumz@gmail.com>
2023-05-01 22:49:56 +0100

    chore: use example

--- pages/api/og.tsx
+++ pages/api/og.tsx
@@ -1,8 +1,7 @@
 import { ImageResponse } from '@vercel/og'
+import { NextRequest } from 'next/server'
 
-export const config = {
-  runtime: 'edge',
-}
+export const config = { runtime: 'edge' }
 
 const fontNormal = fetch(
   new URL('../../src/assets/fonts/Inter-Medium.ttf', import.meta.url),
@@ -11,149 +10,160 @@ const fontBold = fetch(
   new URL('../../src/assets/fonts/Inter-Bold.ttf', import.meta.url),
 ).then((res) => res.arrayBuffer())
 
-export default async function handler(req) {
-  const fontData = await fontNormal
-  const fontBoldData = await fontBold
+export default async function handler(request: NextRequest) {
+  try {
+    const fontData = await fontNormal
+    const fontBoldData = await fontBold
 
-  const { searchParams } = req.nextUrl
+    const { searchParams } = new URL(request.url)
 
-  const title = searchParams.get('title') ?? 'Omar Houmz'
-  const url = searchParams.get('url')
+    const hasTitle = searchParams.has('title')
+    const title = hasTitle
+      ? searchParams.get('title')?.slice(0, 100)
+      : 'My default title'
 
-  return new ImageResponse(
-    (
-      <div
-        style={{
-          display: 'flex',
-          width: '100%',
-          height: '100%',
-          padding: '2.5rem',
-          backgroundColor: '#0f0b64',
-        }}
-      >
+    const url = searchParams.get('url')
+
+    return new ImageResponse(
+      (
         <div
           style={{
             display: 'flex',
-            flexDirection: 'column',
             width: '100%',
             height: '100%',
-            padding: '4rem',
-            backgroundColor: '#fff',
-            borderRadius: '1rem',
+            padding: '2.5rem',
+            backgroundColor: '#0f0b64',
           }}
         >
-          <h1
+          <div
             style={{
-              position: 'relative',
-              fontSize: 68,
-              lineHeight: 1,
-              fontWeight: 700,
-            }}
-          >
-            <span
-              style={{
-                backgroundColor: '#ffef8a',
-                position: 'absolute',
-                width: '5rem',
-                height: '5rem',
-                top: '-2.75rem',
-                left: '-2.75rem',
-                borderRadius: 9999,
-              }}
-            />
-            <span style={{ position: 'relative' }}>{title}</span>
-          </h1>
-          <footer
-            style={{
-              marginTop: 'auto',
               display: 'flex',
-              gap: '1.5rem',
-              alignItems: 'center',
+              flexDirection: 'column',
               width: '100%',
+              height: '100%',
+              padding: '4rem',
+              backgroundColor: '#fff',
+              borderRadius: '1rem',
             }}
           >
-            {/* eslint-disable-next-line @next/next/no-img-element */}
-            <img
-              src='https://www.omarhoumz.com/omar-circle.png'
-              alt=''
-              width={112}
-              height={112}
-              style={{ maxWidth: '100%' }}
-            />
-
-            <div
+            <h1
               style={{
-                display: 'flex',
-                flexDirection: 'column',
-                justifyContent: 'center',
-                marginLeft: 16,
+                position: 'relative',
+                fontSize: 68,
+                lineHeight: 1,
+                fontWeight: 700,
               }}
             >
-              <h3
+              <span
                 style={{
-                  color: '#0f0b64',
-                  fontSize: 24,
-                  fontWeight: 400,
-                  margin: 0,
+                  backgroundColor: '#ffef8a',
+                  position: 'absolute',
+                  width: '5rem',
+                  height: '5rem',
+                  top: '-2.75rem',
+                  left: '-2.75rem',
+                  borderRadius: 9999,
+                }}
+              />
+              <span style={{ position: 'relative' }}>{title}</span>
+            </h1>
+            <footer
+              style={{
+                marginTop: 'auto',
+                display: 'flex',
+                gap: '1.5rem',
+                alignItems: 'center',
+                width: '100%',
+              }}
+            >
+              {/* eslint-disable-next-line @next/next/no-img-element */}
+              <img
+                src='https://www.omarhoumz.com/omar-circle.png'
+                alt=''
+                width={112}
+                height={112}
+                style={{ maxWidth: '100%' }}
+              />
+
+              <div
+                style={{
+                  display: 'flex',
+                  flexDirection: 'column',
+                  justifyContent: 'center',
+                  marginLeft: 16,
                 }}
               >
-                omar
-                <span style={{ fontWeight: 700 }}>houmz</span>
-                .com
-              </h3>
-              {!url ? null : (
                 <h3
                   style={{
-                    fontSize: 34,
-                    backgroundColor: '#f7f3ff',
-                    padding: '0.25rem 0.5rem',
-                    margin: '12px 0 0 0',
+                    color: '#0f0b64',
+                    fontSize: 24,
+                    fontWeight: 400,
+                    margin: 0,
                   }}
                 >
-                  {url}
+                  omar
+                  <span style={{ fontWeight: 700 }}>houmz</span>
+                  .com
                 </h3>
-              )}
-            </div>
+                {!url ? null : (
+                  <h3
+                    style={{
+                      fontSize: 34,
+                      backgroundColor: '#f7f3ff',
+                      padding: '0.25rem 0.5rem',
+                      margin: '12px 0 0 0',
+                    }}
+                  >
+                    {url}
+                  </h3>
+                )}
+              </div>
 
-            {/* eslint-disable-next-line @next/next/no-img-element */}
-            <img
-              src='https://www.omarhoumz.com/icon.png'
-              alt=''
-              width={48}
-              height={48}
-              style={{
-                alignSelf: 'flex-end',
-                marginLeft: 'auto',
-                maxWidth: '100%',
-              }}
-            />
-          </footer>
+              {/* eslint-disable-next-line @next/next/no-img-element */}
+              <img
+                src='https://www.omarhoumz.com/icon.png'
+                alt=''
+                width={48}
+                height={48}
+                style={{
+                  alignSelf: 'flex-end',
+                  marginLeft: 'auto',
+                  maxWidth: '100%',
+                }}
+              />
+            </footer>
+          </div>
         </div>
-      </div>
-    ),
-    {
-      width: 1200,
-      height: 630,
-      fonts: [
-        {
-          name: 'Inter',
-          data: fontData,
-          weight: 400,
-          style: 'normal',
-        },
-        {
-          name: 'Inter',
-          data: fontBoldData,
-          weight: 600,
-          style: 'normal',
-        },
-        // {
-        //   name: 'Inter',
-        //   data: fontBlackData,
-        //   weight: 700,
-        //   style: 'normal',
-        // },
-      ],
-    },
-  )
+      ),
+      {
+        width: 1200,
+        height: 630,
+        fonts: [
+          {
+            name: 'Inter',
+            data: fontData,
+            weight: 400,
+            style: 'normal',
+          },
+          {
+            name: 'Inter',
+            data: fontBoldData,
+            weight: 600,
+            style: 'normal',
+          },
+          // {
+          //   name: 'Inter',
+          //   data: fontBlackData,
+          //   weight: 700,
+          //   style: 'normal',
+          // },
+        ],
+      },
+    )
+  } catch (e: any) {
+    console.log(`${e.message}`)
+    return new Response(`Failed to generate the image`, {
+      status: 500,
+    })
+  }
 }
